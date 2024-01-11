a871323307767045f0a20c4a26c2ebbd134b057d
Ashish Jangra <32738511+AshishJangra27@users.noreply.github.com>
2024-01-11 17:58:41 +0530

    Add files via upload

---
+++ 14. Window, Rank & more/03. saving the results.sql
@@ -0,0 +1,17 @@
+use swiggy;
+select * from restaurants;
+
+-- 1. Creating the table and feeding the values
+create table if not exists tbl (id varchar(255), name varchar(255), city varchar(255), rating float, cost int, cuisine varchar(255), rating_count int, revenue int);
+
+insert into tbl (select id, name, city, rating, cost, cuisine, rating_count,cost*rating_count as 'revenue' from (select *, cost*rating_count ,row_number() over(partition by cuisine order by cost*rating_count desc) as 'rank' from restaurants) t
+where t.rank <= 5);
+select * from tbl;
+
+-- 2. Creating the table directly
+create table if not exists results as (select * from restaurants);
+select * from results;
+
+-- 3. Creating a temporary table
+create temporary table if not exists temp_results as (select * from restaurants);
+select * from temp_results;
