db1726b3e865d0a87d09545049d92930409cc2f7
Ashish Jangra <32738511+AshishJangra27@users.noreply.github.com>
2024-01-12 15:51:51 +0530

    Add files via upload

---
+++ 15. Window & Regex/03. regex.sql
@@ -0,0 +1,12 @@
+use swiggy;
+select * from restaurants;
+
+
+SELECT * FROM restaurants  WHERE name like 's%';
+SELECT * FROM restaurants  WHERE name like '_s%';
+SELECT * FROM restaurants  WHERE name like '%S%';
+SELECT * FROM restaurants WHERE name LIKE '% Pizza%';
+
+SELECT * FROM restaurants WHERE name LIKE '_h_%';
+
+SELECT * FROM restaurants WHERE name REGEXP '^s';
