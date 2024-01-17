723acf15efb6ac1dbb5676091414d132de840e90
Ashish Jangra <32738511+AshishJangra27@users.noreply.github.com>
2024-01-17 09:58:34 +0530

    Add files via upload

---
+++ 16. Getting started with Python/02. datatypes.py
@@ -0,0 +1,52 @@
+# Int
+print(100000)
+print(100)
+print(0)
+print(-10)
+print(-1221897398)
+print(type(12312))
+print('----------------------------')
+
+# Float
+print(100000.5)
+print(100.4)
+print(0.0)
+print(-10.3)
+print(-1221897398.5)
+print(type(0.0))
+print('----------------------------')
+
+# String
+print('A')
+print('a')
+print('Ashish')
+print('Ashish Jangra')
+print('This is a class by GFG')
+print('sdhskjaf hsjkdahfkjsdhfjk hsdkjf h. sdf sdkjfh kjsd')
+print(type("This is Ashish's pen"))
+print('----------------------------')
+
+# Boolean
+print(True)
+print(False)
+print(type('true'))
+print(type('True'))
+print(type(True))
+print('----------------------------')
+
+# Functionalities in print statement
+
+print('a')
+print('b')
+print('c')
+print('a' , 'b', 'c')
+print('----------------------------')
+
+print('a', end = '-')
+print('b', end = '-')
+print('c')
+print('a', 'b','c', end = '*')
+print()
+print('----------------------------')
+
+print('a','b','c', sep = '_____')
