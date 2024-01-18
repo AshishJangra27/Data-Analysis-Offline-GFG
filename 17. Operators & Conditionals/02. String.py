ccf7dc74649237f3f9e6befe195c646c17a40246
Ashish Jangra <32738511+AshishJangra27@users.noreply.github.com>
2024-01-18 18:36:26 +0530

    Add files via upload

---
+++ 17. Operators & Conditionals/02. String.py
@@ -0,0 +1,77 @@
+# Concatenation
+print('Ashish' + ' Jangra')
+print('-------------')
+
+# String Replication
+print('Ashish'*3)
+print('-'*20)
+
+# String Length
+print('Ashish Jangra')
+print(len('Ashish Jangra  '))
+print('-'*20)
+
+# String Slicing
+print('Ashish Jangra')
+print('Ashish'[2])
+print('Ashish'[-6])
+print('Ashish Jangra'[0:6])
+print('Ashish Jangra'[7:13])
+print('Ashish Jangra'[3:6])
+print('Ashish Jangra'[7:13])
+print('Ashish Jangra'[3:13])
+print('Ashish Jangra'[3:])
+print('Ashish Jangra'[:7])
+print('Ashish Jangra'[0:7])
+print('Ashish Jangra'[-5:])
+print('-'*20)
+
+# String Case Conversion
+print('Ashish Jangra'.lower())
+print('Ashish Jangra'.upper())
+print('-'*20)
+
+# String Stripping
+print('       Ashish Jangra   ')
+print('       Ashish Jangra   '.strip())
+print('       Ashish Jangra   '.strip())
+print('-'*20)
+
+# String Replacing
+print('Ashish Jangra')
+print('Ashish Jangra'.replace('h','-'))
+print('-'*20)
+
+# String Count
+print('Ashish Jangra')
+print('Ashish Jangra'.count('a'))
+print('Ashish Jangra'.lower().count('a'))
+print('-'*20)
+
+# String Find
+print('Ashish Jangra')
+print('Ashish Jangra'.find('ish'))
+print('-'*20)
+
+# String Check
+print('Ashish'.isalpha())
+print('123'.isdigit())
+print('ashish'.islower())
+print('ASHISH'.isupper())
+print('-'*20)
+
+# String Capitalization
+print('ashish jangra'.capitalize())
+print('ashish jangra'.title())
+print('-'*20)
+
+# Check for start and end
+print('Ashish Jangra'.startswith("Ash"))
+print('Ashish Jangra'.endswith("gra"))
+print('-'*20)
+
+#
+print('Ashish Jangra'.center(20, "-"))
+print('Ashish Jangra'.ljust(20, "*"))
+print('Ashish Jangra'.rjust(20, "*"))
+print('-'*20)
---
+++ 17. Operators & Conditionals/03. if-else.py
@@ -0,0 +1,16 @@
+n = int(input('Enter the number : '))
+
+if (n == 0):
+    print('its a zero')
+elif (n > 0):
+    print(n ,'is positive')
+else:
+    print(n ,'is negative')
+
+
+if (n == 0):
+    print('its a zero')
+elif (n > 0):
+    print(n ,'is positive')
+else:
+    print(n ,'is negative')
