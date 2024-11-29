6d8a418926086d5de6609994f4208f098df44164
Ashish Jangra <32738511+AshishJangra27@users.noreply.github.com>
2024-11-29 11:13:29 +0530

    Add files via upload

---
+++ DA-06-WD/12. GroupBy/assignment/05. event.sql
@@ -0,0 +1,23 @@
+-- User Specific
+# 1. Which product categories witness the highest sales during the year?
+
+# 2. Which brand witness the highest sales during year?
+
+# 3. Which product categories of a specific brand witnessed maximum sales?
+
+# 4. Which product is witnessed the maximum sales?
+
+-- Merchant Specific
+# 1. What is the average price point of products across different categories and brands?
+# 2. How much discount should be offered on each product to maximize revenue and profit?
+# 3. What is the average discount on tshirt we need to give?
+# 4. What are the biggest brands in tshirt market?
+# 5. How much market of tshirt is captured by roadster?
+# 6. How much market of tshirt is captured by top 5 tshirt brands?
+
+-- Company Specific | Event
+# 1. What kind of content (videos, blogs, influencer engagement) will best resonate with the target audience?
+# 2. Which products should be promoted on the homepage or through personalized recommendations?
+
+-- Company Specific | Improvements
+# 1.  Which products have the highest and lowest ratings and what insights can we glean from them?
