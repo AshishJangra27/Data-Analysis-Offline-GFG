0c2b5666b0c3ca9d3d5cf88fe0809d67156bdcf5
Ashish Jangra <32738511+AshishJangra27@users.noreply.github.com>
2024-12-11 17:53:01 +0530

    Add files via upload

---
+++ DA-06-WD/16. Introduction to Python/Datatypes.py
@@ -0,0 +1,57 @@
+print(14)
+print(3.14)
+print(False)
+print("Ashish")
+
+print(14, 3.14,False,"Ashish")
+
+print("Ashish"     "Jangra" "25" "3.14" "False")
+print("Ashish is a data science MENTOR")
+
+
+print(14, 3.14,False,"Ashish")
+
+print(14 * False * "Ashish")
+
+print('-----------')
+
+
+# Arithmetic Operators
+print(14 + 25)
+print(14 - 25)
+print(14 * 25)
+print(14 ** 3)
+print(26 / 5)
+print(26 // 5)
+print(26 % 5)
+print('-----------')
+
+print(3.14*True)
+print('Ashish'*True)
+print('Ashish'*5)
+
+print('Ashish'*(True*5))
+
+print('Ashish'*2)
+
+print('-'*10)
+
+
+# Int-Int,Float,Bool,Str
+# Float-Int,Float,Bool,Str
+# Bool-Int,Float,Bool,Str
+# Str-Int,Float,Bool,Str
+
+# Comparison Operators
+print(10>=5)
+print(10<5)
+print(10<=5)
+print(5.0==5)
+print(10!=5)
+print(True!=False)
+
+# # Logical Operators
+
+print(((10>5) and (10<11)))
+print(((10>5) or (10<11)))
+print(not(10<11))
---
+++ DA-06-WD/16. Introduction to Python/typecasting.py
@@ -0,0 +1,50 @@
+# int()
+print(int(3.14))
+print(int(True))
+print(int('372439'))
+print('-'*10)
+
+
+# float()
+print(float(3))
+print(float(True))
+print(float('372439'))
+print('-'*10)
+
+
+# str()
+print(str(3))
+print(str(True))
+print(str(3.14))
+print('-'*10)
+
+print(str(3) + str(3))
+print(str(True) + str(False))
+print(str(True) + str(3.15) + str(3))
+print('-'*10)
+
+# bool()
+print(bool(3))
+print(bool('Ashish'))
+print(bool(3.14))
+print(bool(''))
+print(bool("False"))
+print('-'*10)
+
+
+
+print(int(float("3.14")))
+print(float("312"))
+
+
+print('10*5 = ',10*5)
+
+
+
+
+
+
+
+
+
+
