d123344e9c68870f9a93e9a55659b7073dcbb13f
Ashish Jangra <32738511+AshishJangra27@users.noreply.github.com>
2025-01-18 13:15:31 +0530

    Add files via upload

---
+++ DA-08/07. Introduction to Databases/01. basic table.sql
@@ -0,0 +1,34 @@
+-- create database if not exists dabatch;
+
+-- CREATE TABLE IF NOT EXISTS dabatch.users1 (
+--     user_id INT,
+--     username VARCHAR(255));
+    
+    
+-- database gfg
+-- users (name,phone_no,email,location)
+
+drop database if exists gfgs;
+
+create database gfgs;
+
+create table gfgs.users 
+(
+	full_name VARCHAR(20),
+    phone_no VARCHAR(13),
+    email VARCHAR(50),
+    location VARCHAR(255)
+);
+
+
+database trends
+
+-- users (user_name, phone_no, email,user_id, dob,location)
+
+-- inventory (product_id, prod_name,quantity,marked_price,discount_perce, 
+-- 			dom,dop,doe,prod_desc,size,prod_cat)
+--             
+-- transection (transection_id, sale_date_time, marked_price, 
+-- 			discounted_price ,amount_paid,transection_type)
+--             
+
