4473ccd7a881c939ca53b1af7493ed208bdf7093
Ashish Jangra <32738511+AshishJangra27@users.noreply.github.com>
2025-06-07 09:07:15 +0530

    Add files via upload

---
+++ DA-10/11. Joins & Subqueries/06. assignment.sql
@@ -0,0 +1,25 @@
+select * from swiggy.restaurants;
+
+-- 1. Which restaurant of abohar is visied by least number of people?
+
+-- 2. Which restaurant has generated maximum revenue all over india?
+
+-- 3. How many restaurants are having rating more than the average rating?
+
+-- 4. Which restaurant of Delhi has generated most revenue?
+
+-- 5. Which restaurant chain has maximum number of restaurants?
+
+-- 6. Which restaurant chain has generated maximum revenue?
+
+-- 7. Which city has maximum number of restaurants?
+
+-- 8. Which city has generated maximum revenue all over india?
+
+-- 9. List 10 least expensive cuisines?
+
+-- 10. List 10 most expensive cuisines?
+
+-- 11. What is the city is having Biryani as most popular cuisine
+
+-- 12. List top 10 unique restaurants with unique name only thorughout the dataset as per generate maximum revenue (Single restaurant with that name)
