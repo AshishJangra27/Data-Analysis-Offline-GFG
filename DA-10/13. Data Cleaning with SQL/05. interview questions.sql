585e876fdb7dbe8cf97e7b21df1d384e2da2dfbf
Ashish Jangra <32738511+AshishJangra27@users.noreply.github.com>
2025-06-15 10:01:05 +0530

    Add files via upload

---
+++ DA-10/13. Data Cleaning with SQL/05. interview questions.sql
@@ -0,0 +1,33 @@
+-- Basic
+-- 1. What is the difference between SELECT and UPDATE statements?
+-- 2. Explain the use of joins (INNER JOIN, LEFT JOIN, RIGHT JOIN) in SQL.
+-- 3. How do you filter data using WHERE and ORDER BY clauses?
+-- 4. Write a query to find the average order value from an 'orders' table.
+-- 5. List all distinct customer names from a 'customers' table.
+-- 6. What is the difference between Excel & SQL?
+-- 7. What are the different keys we're having in SQL?
+
+-- Intermediate
+-- 1. How do you handle missing data in SQL? (NULL values, empty strings)
+-- SELECT * FROM your_table WHERE column_name IS NULL;
+-- 2. Write a query to find the top 10 most popular products in a 'products' table.
+-- 3. Calculate the sum of sales per salesperson from a 'sales' table with a 'salesperson_id' column.
+-- 4. Explain the difference between queries and subqueries.
+-- 5. Write a query to find customers who haven't placed an order in the last 6 months.
+
+
+-- Advance
+-- 1. What are window functions (e.g., SUM, AVG, ROW_NUMBER) and how are they used?
+-- 2. Explain the concept of normalization?
+-- 3. How do you write a self-join query? Provide an example.
+-- 4. How can you optimize an SQL query for better performance?
+-- 5. Describe the difference between relational databases and NoSQL databases.
+-- 6. What is the difference between partition by, group by & order by?
+-- 7. Write some exmaples of Advanced Window Functions and Joins.
+-- 8. How shuould you design a schema for University Management System?
+
+-- others
+-- Normalization | 1NF, 2NF, 3NF
+-- Performance Optimization
+-- Security in SQL Query
+-- Order of Execution
