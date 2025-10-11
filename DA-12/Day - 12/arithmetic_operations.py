398cba91ffbf6bd5c53f4a74fa6cb40ecba9a017
Ashish Jangra <32738511+AshishJangra27@users.noreply.github.com>
2025-10-11 10:22:48 +0530

    Add files via upload

---
+++ DA-12/Day - 12/arithmetic_operations.py
@@ -0,0 +1,36 @@
+print('Int')
+
+print(12.5 + 5)
+print(12 - 5.2)
+print(1.2 * 5)
+print(12 / 5.2)
+print(13 // 5.2)
+print(13 % 5.5)
+print(12 ** 2.2)
+print('-'*15)
+
+
+print('String')
+print('Ashish' * 5)
+
+
+print('Bool')
+print(False * 'Ashish')
+
+
+print(float(True))
+
+Print = 'a'
+Print = 'b'
+
+print(Print)
+# Int, Float, Bool, String
+# Int- Float, String, Bool
+# Float - Int, Float, String, Bool
+# String - Int, Float, String, Bool
+# Bool - Int, Float, String, Bool
+
+
+
+
+
---
+++ DA-12/Day - 12/print.py
@@ -0,0 +1,14 @@
+print("Int")
+print(-1429387)
+print('----------')
+
+print("Float")
+print(1.0)
+print('----------')
+
+print("String")
+print("1")
+print('----------')
+
+print("String")
+print(False)
