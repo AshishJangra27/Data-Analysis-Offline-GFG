7ce473b42d03ed4862370ae8a9687a93ad809375
Ashish Jangra <32738511+AshishJangra27@users.noreply.github.com>
2025-12-06 15:25:19 +0530

    Update 12. For Loop.py

--- DA-13/12. For Loop.py
+++ DA-13/12. For Loop.py
@@ -16,3 +16,18 @@ for num in range(1,100001):
         
 print('-')
 print(counter)
+
+
+# 2. Probabilities of getting sum of all the number if rolling 4 dices in fractions and percentages
+
+for num in range(4,25):
+    
+    c = 0
+    for i in range(1,7):
+        for j in range(1,7):
+            for k in range(1,7):
+                for l in range(1,7):
+                    if i + j + k + l == num:
+                        c += 1
+                    
+    print(num, ':', str(c) + '/1296', '(',round((c/1296)*100,4), '% )')
